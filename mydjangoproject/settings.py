e6795e2413c66537d2efa860a79b72a07d73fc80
Rojita Lamichhane <r.lamichhane@codavatar.tech>
2022-08-08 21:50:05 +0545

    route modified

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -116,12 +116,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '/var/www/static/',
-# ]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,"static")
+   
+]
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
